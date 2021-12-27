openharmony/third_party_rust_clap

commit c19cc01e6f4e90f5caaa2842d548321755b0fa1c

perf: refactors the POSIX override handling to lazy handling
This commit primarily changes to a lazy handling of POSIX overrides by
relying on github.com/bluss/ordermap instead of the old HashMap impl.
The ordermap allows us to keep track of which arguments arrived first,
and therefore determine which ones should be removed when an override
conflict is found.

This has the added benefit of we no longer have to do the bookkeeping to
keep track and override args as they come in, we can do it once at the
end.

Finally, ordermap allows fast Vec like iteration of the keys, which we
end up doing several times. Benching is still TBD once the v3 prep is
done, but this change should have a meaningful impact.

src/app/parser.rs
@@ -47,7 +47,7 @@ where
     pub app: &'c mut App<'a, 'b>,
     pub required: Vec<&'a str>,
     pub r_ifs: Vec<(&'a str, &'b str, &'a str)>,
-    pub overrides: Vec<(&'b str, &'a str)>,
+    pub overriden: Vec<&'a str>,
     cache: Option<&'a str>,
     num_opts: usize,
     num_flags: usize,
@@ -97,7 +97,7 @@ where
             app: app,
             required: reqs,
             r_ifs: Vec::new(),
-            overrides: Vec::new(),
+            overriden: Vec::new(),
             cache: None,
             num_opts: 0,
             num_flags: 0,
@@ -390,14 +390,14 @@ where
                 }
 
                 if starts_new_arg {
-                    {
-                        let any_arg = find!(self.app, &self.cache.unwrap_or(""));
-                        matcher.process_arg_overrides(
-                            any_arg,
-                            &mut self.overrides,
-                            &mut self.required,
-                        );
-                    }
+                    // {
+                    //     let any_arg = find!(self.app, &self.cache.unwrap_or(""));
+                    //     matcher.process_arg_overrides(
+                    //         any_arg,
+                    //         &mut self.overrides,
+                    //         &mut self.required,
+                    //     );
+                    // }
 
                     if arg_os.starts_with(b"--") {
                         needs_val_of = self.parse_long_arg(matcher, &arg_os)?;
@@ -527,14 +527,14 @@ where
                     self.app.settings.set(AS::TrailingValues);
                 }
                 if self.cache.map_or(true, |name| name != p.name) {
-                    {
-                        let any_arg = find!(self.app, &self.cache.unwrap_or(""));
-                        matcher.process_arg_overrides(
-                            any_arg,
-                            &mut self.overrides,
-                            &mut self.required,
-                        );
-                    }
+                    // {
+                    //     let any_arg = find!(self.app, &self.cache.unwrap_or(""));
+                    //     matcher.process_arg_overrides(
+                    //         any_arg,
+                    //         &mut self.overrides,
+                    //         &mut self.required,
+                    //     );
+                    // }
                     self.cache = Some(p.name);
                 }
                 let _ = self.add_val_to_arg(p, &arg_os, matcher)?;
@@ -647,10 +647,10 @@ where
         }
 
         // In case the last arg was new, we  need to process it's overrides
-        {
-            let any_arg = find!(self.app, &self.cache.unwrap_or(""));
-            matcher.process_arg_overrides(any_arg, &mut self.overrides, &mut self.required);
-        }
+        // {
+        //     let any_arg = find!(self.app, &self.cache.unwrap_or(""));
+        //     matcher.process_arg_overrides(any_arg, &mut self.overrides, &mut self.required);
+        // }
 
         self.remove_overrides(matcher);
 
@@ -804,30 +804,31 @@ where
     }
 
     fn remove_overrides(&mut self, matcher: &mut ArgMatcher) {
-        debugln!("Parser::remove_overrides:{:?};", self.overrides);
-        for &(overr, name) in &self.overrides {
-            debugln!("Parser::remove_overrides:iter:({},{});", overr, name);
-            if matcher.is_present(overr) {
-                debugln!(
-                    "Parser::remove_overrides:iter:({},{}): removing {};",
-                    overr,
-                    name,
-                    name
-                );
-                matcher.remove(name);
-                for i in (0..self.required.len()).rev() {
-                    debugln!(
-                        "Parser::remove_overrides:iter:({},{}): removing required {};",
-                        overr,
-                        name,
-                        name
-                    );
-                    if self.required[i] == name {
-                        self.required.swap_remove(i);
-                        break;
+        debugln!("Parser::remove_overrides;");
+        let mut to_rem: Vec<&str> = Vec::new();
+        let mut seen: Vec<&str> = Vec::new();
+        for name in matcher.arg_names() {
+            debugln!("Parser::remove_overrides:iter:{};", name);
+            if let Some(arg) = find!(self.app, name) {
+                if let Some(ref overrides) = arg.overrides {
+                    debugln!("Parser::remove_overrides:iter:{}:{:?};", name, overrides);
+                    for o in overrides {
+                        if matcher.is_present(o) && !seen.contains(o) {
+                            debugln!("Parser::remove_overrides:iter:{}:iter:{}: self;", name, o);
+                            to_rem.push(arg.name);
+                        } else {
+                            debugln!("Parser::remove_overrides:iter:{}:iter:{}: other;", name, o);
+                            to_rem.push(o);
+                        }
                     }
                 }
+                seen.push(arg.name);
             }
+        }
+        for name in &to_rem {
+            debugln!("Parser::remove_overrides:iter:{}: removing;", name);
+            matcher.remove(name);
+            self.overriden.push(name);
         }
     }
 

src/app/validator.rs
@@ -146,7 +146,7 @@ impl<'a, 'b, 'c, 'z> Validator<'a, 'b, 'c, 'z> {
         Ok(())
     }
 
-    fn build_err(&self, name: &str, matcher: &ArgMatcher<'a>) -> ClapResult<()> {
+    fn build_conflict_err(&self, name: &str, matcher: &ArgMatcher<'a>) -> ClapResult<()> {
         debugln!("build_err!: name={}", name);
         let mut c_with = find_from!(self.0.app, &name, blacklist, &matcher);
         c_with = c_with.or(find!(self.0.app, &name)
@@ -172,78 +172,24 @@ impl<'a, 'b, 'c, 'z> Validator<'a, 'b, 'c, 'z> {
 
     fn validate_blacklist(&self, matcher: &mut ArgMatcher<'a>) -> ClapResult<()> {
         debugln!("Validator::validate_blacklist;");
-        let mut conflicts: Vec<&str> = vec![];
-        for (&name, _) in matcher.iter() {
+        for name in &self.gather_conflicts(matcher) {
             debugln!("Validator::validate_blacklist:iter:{};", name);
-            if let Some(grps) = self.0.groups_for_arg(name) {
-                for grp in &grps {
-                    if let Some(g) = self.0.app.groups.iter().find(|g| &g.name == grp) {
-                        if !g.multiple {
-                            for arg in &g.args {
-                                if arg == &name {
-                                    continue;
-                                }
-                                conflicts.push(arg);
-                            }
-                        }
-                        if let Some(ref gc) = g.conflicts {
-                            conflicts.extend(&*gc);
-                        }
-                    }
-                }
-            }
-            if let Some(arg) = find!(self.0.app, &name) {
-                if let Some(ref bl) = arg.blacklist {
-                    for conf in bl {
-                        if matcher.get(conf).is_some() {
-                            conflicts.push(conf);
-                        }
-                    }
-                }
-            } else {
-                debugln!("Validator::validate_blacklist:iter:{}:group;", name);
-                let args = self.0.arg_names_in_group(name);
-                for arg in &args {
-                    debugln!(
-                        "Validator::validate_blacklist:iter:{}:group:iter:{};",
-                        name,
-                        arg
-                    );
-                    if let Some(ref bl) = find!(self.0.app, arg).unwrap().blacklist {
-                        for conf in bl {
-                            if matcher.get(conf).is_some() {
-                                conflicts.push(conf);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        for name in &conflicts {
-            debugln!(
-                "Validator::validate_blacklist:iter:{}: Checking blacklisted arg",
-                name
-            );
             let mut should_err = false;
             if groups!(self.0.app).any(|g| &g.name == name) {
-                debugln!(
-                    "Validator::validate_blacklist:iter:{}: groups contains it...",
-                    name
-                );
+                debugln!("Validator::validate_blacklist:iter:{}:group;", name);
                 for n in self.0.arg_names_in_group(name) {
                     debugln!(
-                        "Validator::validate_blacklist:iter:{}:iter:{}: looking in group...",
+                        "Validator::validate_blacklist:iter:{}:group:iter:{};",
                         name,
                         n
                     );
                     if matcher.contains(n) {
                         debugln!(
-                            "Validator::validate_blacklist:iter:{}:iter:{}: matcher contains it...",
+                            "Validator::validate_blacklist:iter:{}:group:iter:{}: found;",
                             name,
                             n
                         );
-                        return self.build_err(n, matcher);
+                        return self.build_conflict_err(n, matcher);
                     }
                 }
             } else if let Some(ma) = matcher.get(name) {
@@ -254,10 +200,64 @@ impl<'a, 'b, 'c, 'z> Validator<'a, 'b, 'c, 'z> {
                 should_err = ma.occurs > 0;
             }
             if should_err {
-                return self.build_err(*name, matcher);
+                return self.build_conflict_err(*name, matcher);
             }
         }
         Ok(())
+    }
+
+    fn gather_conflicts(&self, matcher: &mut ArgMatcher<'a>) -> Vec<&'a str> {
+        debugln!("Validator::gather_conflicts;");
+        let mut conflicts = vec![];
+        for name in matcher.arg_names() {
+            debugln!("Validator::gather_conflicts:iter:{};", name);
+            if let Some(arg) = find!(self.0.app, name) {
+                if let Some(ref bl) = arg.blacklist {
+                    for conf in bl {
+                        if matcher.get(conf).is_some() {
+                            conflicts.push(*conf);
+                        }
+                    }
+                }
+                if let Some(grps) = self.0.groups_for_arg(name) {
+                    for grp in &grps {
+                        if let Some(g) = find!(self.0.app, grp, groups) {
+                            if !g.multiple {
+                                for g_arg in &g.args {
+                                    if &g_arg == &name {
+                                        continue;
+                                    }
+                                    conflicts.push(g_arg);
+                                }
+                            }
+                            if let Some(ref gc) = g.conflicts {
+                                conflicts.extend(&*gc);
+                            }
+                        }
+                    }
+                }
+            } else {
+                debugln!("Validator::gather_conflicts:iter:{}:group;", name);
+                let args = self.0.arg_names_in_group(name);
+                for arg in &args {
+                    debugln!(
+                        "Validator::gather_conflicts:iter:{}:group:iter:{};",
+                        name,
+                        arg
+                    );
+                    if let Some(ref bl) =
+                        find!(self.0.app, arg).expect(INTERNAL_ERROR_MSG).blacklist
+                    {
+                        for conf in bl {
+                            if matcher.get(conf).is_some() {
+                                conflicts.push(conf);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        conflicts
     }
 
     fn validate_matched_args(&self, matcher: &mut ArgMatcher<'a>) -> ClapResult<()> {
@@ -438,6 +438,13 @@ impl<'a, 'b, 'c, 'z> Validator<'a, 'b, 'c, 'z> {
         Ok(())
     }
 
+    fn is_missing_required_ok(&self, a: &Arg<'a, 'b>, matcher: &ArgMatcher<'a>) -> bool {
+        debugln!("Validator::is_missing_required_ok: a={}", a.name);
+        self.validate_arg_conflicts(a, matcher).unwrap_or(false)
+            || self.validate_required_unless(a, matcher).unwrap_or(false)
+            || self.0.overriden.contains(&a.name)
+    }
+
     fn validate_arg_conflicts(&self, a: &Arg<'a, 'b>, matcher: &ArgMatcher<'a>) -> Option<bool> {
         debugln!("Validator::validate_arg_conflicts: a={:?};", a.name);
         a.blacklist.as_ref().map(|bl| {
@@ -505,12 +512,5 @@ impl<'a, 'b, 'c, 'z> Validator<'a, 'b, 'c, 'z> {
             &*Usage::new(self.0).create_error_usage(matcher, extra),
             self.0.app.color(),
         ))
-    }
-
-    #[inline]
-    fn is_missing_required_ok(&self, a: &Arg<'a, 'b>, matcher: &ArgMatcher<'a>) -> bool {
-        debugln!("Validator::is_missing_required_ok: a={}", a.name);
-        self.validate_arg_conflicts(a, matcher).unwrap_or(false)
-            || self.validate_required_unless(a, matcher).unwrap_or(false)
     }
 }

Cargo.toml
@@ -23,6 +23,7 @@ appveyor = { repository = "kbknapp/clap-rs" }
 bitflags              = "1.0"
 unicode-width         = "0.1.4"
 textwrap              = "0.9.0"
+ordermap			  = "0.3.5"
 strsim    = { version = "0.7.0",  optional = true }
 ansi_term = { version = "0.10.0",  optional = true }
 yaml-rust = { version = "0.3.5",  optional = true }

src/app/usage.rs
@@ -31,7 +31,6 @@ impl<'a, 'b, 'c, 'z> Usage<'a, 'b, 'c, 'z> {
     pub fn create_error_usage(&self, matcher: &ArgMatcher<'a>, extra: Option<&str>) -> String {
         let mut args: Vec<_> = matcher
             .arg_names()
-            .iter()
             .filter(|ref n| {
                 if let Some(a) = find!(self.0.app, **n) {
                     !a.is_set(ArgSettings::Required) && !a.is_set(ArgSettings::Hidden)

src/args/arg_matcher.rs
@@ -1,9 +1,10 @@
 // Std
-use std::collections::hash_map::{Entry, Iter};
-use std::collections::HashMap;
 use std::ffi::OsStr;
-use std::ops::Deref;
+use std::collections::HashMap;
 use std::mem;
+
+// Third Party
+use ordermap;
 
 // Internal
 use args::{Arg, ArgMatches, MatchedArg, SubCommand};
@@ -19,44 +20,6 @@ impl<'a> Default for ArgMatcher<'a> {
 
 impl<'a> ArgMatcher<'a> {
     pub fn new() -> Self { ArgMatcher::default() }
-
-    pub fn process_arg_overrides<'b>(
-        &mut self,
-        a: Option<&Arg<'a, 'b>>,
-        overrides: &mut Vec<(&'b str, &'a str)>,
-        required: &mut Vec<&'a str>,
-    ) {
-        debugln!(
-            "ArgMatcher::process_arg_overrides:{:?};",
-            a.map_or(None, |a| Some(a.name))
-        );
-        if let Some(aa) = a {
-            if let Some(ref a_overrides) = aa.overrides {
-                for overr in a_overrides {
-                    debugln!("ArgMatcher::process_arg_overrides:iter:{};", overr);
-                    if self.is_present(overr) {
-                        debugln!(
-                            "ArgMatcher::process_arg_overrides:iter:{}: removing from matches;",
-                            overr
-                        );
-                        self.remove(overr);
-                        for i in (0..required.len()).rev() {
-                            if &required[i] == overr {
-                                debugln!(
-                                    "ArgMatcher::process_arg_overrides:iter:{}: removing required;",
-                                    overr
-                                );
-                                required.swap_remove(i);
-                                break;
-                            }
-                        }
-                    } else {
-                        overrides.push((overr, aa.name));
-                    }
-                }
-            }
-        }
-    }
 
     pub fn is_present(&self, name: &str) -> bool { self.0.is_present(name) }
 
@@ -126,15 +89,17 @@ impl<'a> ArgMatcher<'a> {
 
     pub fn usage(&mut self, usage: String) { self.0.usage = Some(usage); }
 
-    pub fn arg_names(&'a self) -> Vec<&'a str> { self.0.args.keys().map(Deref::deref).collect() }
+    pub fn arg_names(&'a self) -> ordermap::Keys<&'a str, MatchedArg> { self.0.args.keys() }
 
-    pub fn entry(&mut self, arg: &'a str) -> Entry<&'a str, MatchedArg> { self.0.args.entry(arg) }
+    pub fn entry(&mut self, arg: &'a str) -> ordermap::Entry<&'a str, MatchedArg> {
+        self.0.args.entry(arg)
+    }
 
     pub fn subcommand(&mut self, sc: SubCommand<'a>) { self.0.subcommand = Some(Box::new(sc)); }
 
     pub fn subcommand_name(&self) -> Option<&str> { self.0.subcommand_name() }
 
-    pub fn iter(&self) -> Iter<&str, MatchedArg> { self.0.args.iter() }
+    pub fn iter(&self) -> ordermap::Iter<&str, MatchedArg> { self.0.args.iter() }
 
     pub fn inc_occurrence_of(&mut self, arg: &'a str) {
         debugln!("ArgMatcher::inc_occurrence_of: arg={}", arg);

src/args/arg_matches.rs
@@ -1,9 +1,11 @@
 // Std
 use std::borrow::Cow;
-use std::collections::HashMap;
 use std::ffi::{OsStr, OsString};
 use std::iter::Map;
 use std::slice::Iter;
+
+// Third Party
+use ordermap::OrderMap;
 
 // Internal
 use INVALID_UTF8;
@@ -60,7 +62,7 @@ use args::SubCommand;
 #[derive(Debug, Clone)]
 pub struct ArgMatches<'a> {
     #[doc(hidden)]
-    pub args: HashMap<&'a str, MatchedArg>,
+    pub args: OrderMap<&'a str, MatchedArg>,
     #[doc(hidden)]
     pub subcommand: Option<Box<SubCommand<'a>>>,
     #[doc(hidden)]
@@ -70,7 +72,7 @@ pub struct ArgMatches<'a> {
 impl<'a> Default for ArgMatches<'a> {
     fn default() -> Self {
         ArgMatches {
-            args: HashMap::new(),
+            args: OrderMap::new(),
             subcommand: None,
             usage: None,
         }

src/lib.rs
@@ -534,6 +534,7 @@ extern crate ansi_term;
 extern crate atty;
 #[macro_use]
 extern crate bitflags;
+extern crate ordermap;
 #[cfg(feature = "suggestions")]
 extern crate strsim;
 #[cfg(feature = "wrap_help")]

src/macros.rs
@@ -955,7 +955,7 @@ macro_rules! find_from {
     ($app:expr, $arg_name:expr, $from:ident, $matcher:expr) => {{
         let mut ret = None;
         for k in $matcher.arg_names() {
-            if let Some(a) = find!($app, &k) {
+            if let Some(a) = find!($app, k) {
                 if let Some(ref v) = a.$from {
                     if v.contains($arg_name) {
                         ret = Some(a.to_string());

commit c1a4c18b3b8181419e984096086000a40208a61a

fix: Remove overriden occurrences as we go
This allows two overriding args to interact with each other mid-line.
This was broken in c19cc01 / #1154.  Before that, we duplicated the override
logic in several places.

Now we plug into the start of a new arg which allows us to do this
incrementally without making the logic complex or inefficient, thanks to
prior refactors.

Fixes #3217

src/parse/matches/matched_arg.rs
@@ -108,13 +108,6 @@ impl MatchedArg {
         }
     }
 
-    pub(crate) fn override_vals(&mut self) {
-        let len = self.vals.len();
-        if len > 1 {
-            self.vals.drain(0..len - 1);
-        }
-    }
-
     pub(crate) fn contains_val(&self, val: &str) -> bool {
         self.vals_flatten().any(|v| {
             if self.ignore_case {
@@ -161,47 +154,6 @@ pub(crate) enum ValueType {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_vals_override() {
-        let mut m = MatchedArg::new();
-        m.push_val("aaa".into());
-        m.new_val_group();
-        m.append_val("bbb".into());
-        m.append_val("ccc".into());
-        m.new_val_group();
-        m.append_val("ddd".into());
-        m.push_val("eee".into());
-        m.new_val_group();
-        m.append_val("fff".into());
-        m.append_val("ggg".into());
-        m.append_val("hhh".into());
-        m.append_val("iii".into());
-
-        m.override_vals();
-        let vals: Vec<&Vec<OsString>> = m.vals().collect();
-        assert_eq!(
-            vals,
-            vec![&vec![
-                OsString::from("fff"),
-                OsString::from("ggg"),
-                OsString::from("hhh"),
-                OsString::from("iii"),
-            ]]
-        );
-        m.override_vals();
-
-        let vals: Vec<&Vec<OsString>> = m.vals().collect();
-        assert_eq!(
-            vals,
-            vec![&vec![
-                OsString::from("fff"),
-                OsString::from("ggg"),
-                OsString::from("hhh"),
-                OsString::from("iii"),
-            ]]
-        );
-    }
 
     #[test]
     fn test_grouped_vals_first() {

src/parse/parser.rs
@@ -1,6 +1,6 @@
 // Std
 use std::{
-    cell::Cell,
+    cell::{Cell, RefCell},
     ffi::{OsStr, OsString},
 };
 
@@ -26,7 +26,7 @@ use crate::{
 pub(crate) struct Parser<'help, 'app> {
     pub(crate) app: &'app mut App<'help>,
     pub(crate) required: ChildGraph<Id>,
-    pub(crate) overridden: Vec<Id>,
+    pub(crate) overridden: RefCell<Vec<Id>>,
     pub(crate) seen: Vec<Id>,
     pub(crate) cur_idx: Cell<usize>,
     /// Index of the previous flag subcommand in a group of flags.
@@ -51,7 +51,7 @@ impl<'help, 'app> Parser<'help, 'app> {
         Parser {
             app,
             required: reqs,
-            overridden: Vec::new(),
+            overridden: Default::default(),
             seen: Vec::new(),
             cur_idx: Cell::new(0),
             flag_subcmd_at: None,
@@ -657,8 +657,6 @@ impl<'help, 'app> Parser<'help, 'app> {
                     matches: sc_m.into_inner(),
                 });
 
-                self.remove_overrides(matcher);
-
                 return Validator::new(self).validate(
                     parse_state,
                     subcmd_name.is_some(),
@@ -695,8 +693,6 @@ impl<'help, 'app> Parser<'help, 'app> {
                 self.app.settings.is_set(AS::WaitOnError),
             ));
         }
-
-        self.remove_overrides(matcher);
 
         Validator::new(self).validate(parse_state, subcmd_name.is_some(), matcher, trailing_values)
     }
@@ -1475,60 +1471,27 @@ impl<'help, 'app> Parser<'help, 'app> {
         ParseResult::ValuesDone
     }
 
-    fn remove_overrides(&mut self, matcher: &mut ArgMatcher) {
-        debug!("Parser::remove_overrides");
-        let mut to_rem: Vec<Id> = Vec::new();
-        let mut self_override: Vec<Id> = Vec::new();
-        let mut arg_overrides = Vec::new();
-        for name in matcher.arg_names() {
-            debug!("Parser::remove_overrides:iter:{:?}", name);
-            if let Some(overrider) = self.app.find(name) {
-                let mut override_self = false;
-                for overridee in &overrider.overrides {
-                    debug!(
-                        "Parser::remove_overrides:iter:{:?} => {:?}",
-                        name, overrider
-                    );
-                    if *overridee == overrider.id {
-                        override_self = true;
-                    } else {
-                        arg_overrides.push((overrider.id.clone(), overridee));
-                        arg_overrides.push((overridee.clone(), &overrider.id));
-                    }
-                }
-                if override_self {
-                    debug!(
-                        "Parser::remove_overrides:iter:{:?}:iter:{:?}: self override",
-                        name, overrider
-                    );
-                    self_override.push(overrider.id.clone());
+    fn remove_overrides(&self, arg: &Arg<'help>, matcher: &mut ArgMatcher) {
+        debug!("Parser::remove_overrides: id={:?}", arg.id);
+        for override_id in &arg.overrides {
+            debug!("Parser::remove_overrides:iter:{:?}: removing", override_id);
+            matcher.remove(override_id);
+            self.overridden.borrow_mut().push(override_id.clone());
+        }
+
+        // Override anything that can override us
+        let mut transitive = Vec::new();
+        for arg_id in matcher.arg_names() {
+            if let Some(overrider) = self.app.find(arg_id) {
+                if overrider.overrides.contains(&arg.id) {
+                    transitive.push(&overrider.id);
                 }
             }
         }
-
-        // remove future overrides in reverse seen order
-        for arg in self.seen.iter().rev() {
-            for (a, overr) in arg_overrides.iter().filter(|(a, _)| a == arg) {
-                if !to_rem.contains(a) {
-                    to_rem.push((*overr).clone());
-                }
-            }
-        }
-
-        // Do self overrides
-        for name in &self_override {
-            debug!("Parser::remove_overrides:iter:self:{:?}: resetting", name);
-            if let Some(ma) = matcher.get_mut(name) {
-                ma.occurs = 1;
-                ma.override_vals();
-            }
-        }
-
-        // Finally remove conflicts
-        for name in &to_rem {
-            debug!("Parser::remove_overrides:iter:{:?}: removing", name);
-            matcher.remove(name);
-            self.overridden.push(name.clone());
+        for overrider_id in transitive {
+            debug!("Parser::remove_overrides:iter:{:?}: removing", overrider_id);
+            matcher.remove(overrider_id);
+            self.overridden.borrow_mut().push(overrider_id.clone());
         }
     }
 
@@ -1726,6 +1689,9 @@ impl<'help, 'app> Parser<'help, 'app> {
 
     /// Increase occurrence of specific argument and the grouped arg it's in.
     fn inc_occurrence_of_arg(&self, matcher: &mut ArgMatcher, arg: &Arg<'help>) {
+        // With each new occurrence, remove overrides from prior occurrences
+        self.remove_overrides(arg, matcher);
+
         matcher.inc_occurrence_of_arg(arg);
         // Increment or create the group "args"
         for group in self.app.groups_for_arg(&arg.id) {

src/parse/validator.rs
@@ -457,7 +457,7 @@ impl<'help, 'app, 'parser> Validator<'help, 'app, 'parser> {
 
     fn is_missing_required_ok(&self, a: &Arg<'help>, matcher: &ArgMatcher) -> bool {
         debug!("Validator::is_missing_required_ok: {}", a.name);
-        self.validate_arg_conflicts(a, matcher) || self.p.overridden.contains(&a.id)
+        self.validate_arg_conflicts(a, matcher) || self.p.overridden.borrow().contains(&a.id)
     }
 
     fn validate_arg_conflicts(&self, a: &Arg<'help>, matcher: &ArgMatcher) -> bool {

tests/builder/posix_compatible.rs
@@ -387,3 +387,15 @@ fn issue_1374_overrides_self_with_multiple_values() {
         &["c", "d"]
     );
 }
+
+#[test]
+fn incremental_override() {
+    let mut app = App::new("test")
+        .arg(arg!(--name <NAME>).multiple_occurrences(true))
+        .arg(arg!(--"no-name").overrides_with("name"));
+    let m = app
+        .try_get_matches_from_mut(&["test", "--name=ahmed", "--no-name", "--name=ali"])
+        .unwrap();
+    assert_eq!(m.values_of("name").unwrap().collect::<Vec<_>>(), &["ali"]);
+    assert!(!m.is_present("no-name"));
+}