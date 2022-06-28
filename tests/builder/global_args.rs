openharmony/third_party_rust_clap

commit 8f4f138c086ccd15afad1149c2cae6ab0027dc1f

refactor(parser): Be explicit in checking present values

src/parser/arg_matcher.rs
@@ -70,11 +70,13 @@ impl ArgMatcher {
                 // We have to check if the parent's global arg wasn't used but still exists
                 // such as from a default value.
                 //
-                // For example, `myprog subcommand --global-arg=value` where --global-arg defines
+                // For example, `myprog subcommand --global-arg=value` where `--global-arg` defines
                 // a default value of `other` myprog would have an existing MatchedArg for
-                // --global-arg where the value is `other`, however the occurs will be 0.
+                // `--global-arg` where the value is `other`
                 let to_update = if let Some(parent_ma) = vals_map.get(global_arg) {
-                    if parent_ma.get_occurrences() > 0 && ma.get_occurrences() == 0 {
+                    if parent_ma.check_explicit(ArgPredicate::IsPresent)
+                        && !ma.check_explicit(ArgPredicate::IsPresent)
+                    {
                         parent_ma
                     } else {
                         ma

commit f812945862739fdbe87eba0b33dffbf2de3a664a

fix(parser): Ensure globals override env vars
This fixes a bug introduced in 8f4f138c086ccd15afad1149c2cae6ab0027dc1f
when we were trying to move away from presence checks via occurrences.
I switched it to the common type of presence check but really what we
want is a highest-precedence check.

Fixes #3872

src/parser/arg_matcher.rs
@@ -74,9 +74,7 @@ impl ArgMatcher {
                 // a default value of `other` myprog would have an existing MatchedArg for
                 // `--global-arg` where the value is `other`
                 let to_update = if let Some(parent_ma) = vals_map.get(global_arg) {
-                    if parent_ma.check_explicit(ArgPredicate::IsPresent)
-                        && !ma.check_explicit(ArgPredicate::IsPresent)
-                    {
+                    if parent_ma.source() > ma.source() {
                         parent_ma
                     } else {
                         ma

tests/builder/global_args.rs
@@ -163,3 +163,40 @@ fn global_overrides_default() {
         .unwrap();
     assert_eq!(m.get_one::<String>("name").unwrap().as_str(), "from_arg");
 }
+
+#[test]
+#[cfg(feature = "env")]
+fn global_overrides_env() {
+    std::env::set_var("GLOBAL_OVERRIDES_ENV", "from_env");
+
+    let cmd = Command::new("test")
+        .arg(
+            Arg::new("name")
+                .long("name")
+                .global(true)
+                .takes_value(true)
+                .env("GLOBAL_OVERRIDES_ENV"),
+        )
+        .subcommand(Command::new("sub"));
+
+    let m = cmd.clone().try_get_matches_from(["test"]).unwrap();
+    assert_eq!(m.get_one::<String>("name").unwrap().as_str(), "from_env");
+
+    let m = cmd
+        .clone()
+        .try_get_matches_from(["test", "--name", "from_arg"])
+        .unwrap();
+    assert_eq!(m.get_one::<String>("name").unwrap().as_str(), "from_arg");
+
+    let m = cmd
+        .clone()
+        .try_get_matches_from(["test", "--name", "from_arg", "sub"])
+        .unwrap();
+    assert_eq!(m.get_one::<String>("name").unwrap().as_str(), "from_arg");
+
+    let m = cmd
+        .clone()
+        .try_get_matches_from(["test", "sub", "--name", "from_arg"])
+        .unwrap();
+    assert_eq!(m.get_one::<String>("name").unwrap().as_str(), "from_arg");
+}