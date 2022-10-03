openharmony/third_party_rust_clap

commit faacbd9b08fa70e616091a806200d56154dface2

perf(derive): Only parse doc comments when needed for field/variant

clap_derive/src/item.rs
@@ -130,7 +130,9 @@ impl Item {
         let parsed_attrs = ClapAttr::parse_all(&variant.attrs);
         res.infer_kind(&parsed_attrs);
         res.push_attrs(&parsed_attrs);
-        res.push_doc_comment(&variant.attrs, "about");
+        if matches!(&*res.kind, Kind::Command(_)) {
+            res.push_doc_comment(&variant.attrs, "about");
+        }
 
         match &*res.kind {
             Kind::Flatten => {
@@ -140,9 +142,6 @@ impl Item {
                         "methods are not allowed for flattened entry"
                     );
                 }
-
-                // ignore doc comments
-                res.doc_comment = vec![];
             }
 
             Kind::Subcommand(_)
@@ -174,7 +173,9 @@ impl Item {
         let parsed_attrs = ClapAttr::parse_all(&variant.attrs);
         res.infer_kind(&parsed_attrs);
         res.push_attrs(&parsed_attrs);
-        res.push_doc_comment(&variant.attrs, "help");
+        if matches!(&*res.kind, Kind::Value) {
+            res.push_doc_comment(&variant.attrs, "help");
+        }
 
         res
     }
@@ -198,7 +199,9 @@ impl Item {
         let parsed_attrs = ClapAttr::parse_all(&field.attrs);
         res.infer_kind(&parsed_attrs);
         res.push_attrs(&parsed_attrs);
-        res.push_doc_comment(&field.attrs, "help");
+        if matches!(&*res.kind, Kind::Arg(_)) {
+            res.push_doc_comment(&field.attrs, "help");
+        }
 
         match &*res.kind {
             Kind::Flatten => {
@@ -208,9 +211,6 @@ impl Item {
                         "methods are not allowed for flattened entry"
                     );
                 }
-
-                // ignore doc comments
-                res.doc_comment = vec![];
             }
 
             Kind::Subcommand(_) => {

commit aea7d50003cbce7775039019efa9f87f16aee369

fix(derive): Apply doc comments for `#[command(subcommand)]`
This was broken in faacbd9b08fa70e616091a806200d56154dface2.  I had
missed this case for parsing doc comments.

clap_derive/src/item.rs
@@ -143,7 +143,7 @@ impl Item {
         let parsed_attrs = ClapAttr::parse_all(&variant.attrs);
         res.infer_kind(&parsed_attrs);
         res.push_attrs(&parsed_attrs);
-        if matches!(&*res.kind, Kind::Command(_)) {
+        if matches!(&*res.kind, Kind::Command(_) | Kind::Subcommand(_)) {
             res.push_doc_comment(&variant.attrs, "about", true);
         }
 
@@ -155,6 +155,9 @@ impl Item {
                         "methods are not allowed for flattened entry"
                     );
                 }
+
+                // ignore doc comments
+                res.doc_comment = vec![];
             }
 
             Kind::Subcommand(_)
@@ -228,6 +231,9 @@ impl Item {
                         "methods are not allowed for flattened entry"
                     );
                 }
+
+                // ignore doc comments
+                res.doc_comment = vec![];
             }
 
             Kind::Subcommand(_) => {

tests/derive/doc_comments_help.rs
@@ -268,7 +268,7 @@ fn respect_subcommand_doc_comment() {
 Usage: cmd <COMMAND>
 
 Commands:
-  child  
+  child  For child
   help   Print this message or the help of the given subcommand(s)
 
 Options: